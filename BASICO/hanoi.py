c6853b369d2ef300089b4e39014178d93e65811a
codenmicole <31217295+codenmicole@users.noreply.github.com>
2022-06-22 12:24:15 -0500

    Add files via upload

---
+++ BASICO/hanoi.py
@@ -0,0 +1,11 @@
+def Hanoi (discos,TorreOrigen=1,TorreAuxiliar=2,TorreDestino=3):
+    if discos==1:
+        print (TorreOrigen,"a",TorreDestino)
+        
+    else:
+        Hanoi(discos-1,TorreOrigen,TorreDestino,TorreAuxiliar)
+        print(TorreOrigen,"a",TorreDestino)
+        Hanoi (discos-1,TorreAuxiliar,TorreOrigen,TorreDestino)
+    return
+
+Hanoi(5)
